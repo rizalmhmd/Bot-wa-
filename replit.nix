373946aabedf4b47bc5892f4ac33478eb34d1843
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-12-21 09:52:59 +0700

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,9 +1,18 @@
 { pkgs }: {
     deps = [
-        pkgs.nodejs
-        pkgs.nodePackages.typescript
+
+        pkgs.nodejs-16_x
+        pkgs.libwebp
+        pkgs.python
+	      pkgs.nodePackages.typescript
+        pkgs.libuuid
         pkgs.ffmpeg
-        pkgs.imagemagick
+        pkgs.imagemagick  
+        pkgs.wget
         pkgs.git
+        pkgs.nodePackages.pm2
     ];
+  env ={
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];
+  };
 }
